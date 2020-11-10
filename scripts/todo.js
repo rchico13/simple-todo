e45f057950b44fdf6727bea159275b29102ab4db
scottgc <scott.gc@gmail.com>
2020-11-09 21:02:07 -0700

    Add method to capture todo item from input element.

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,3 +1,11 @@
 function sendAlert(message) {
   alert(message);
 }
+
+function addTodoItem() {
+  var todo = document.getElementById("todo-item");
+
+  this.sendAlert(todo.value);
+
+  todo.value = null
+}
