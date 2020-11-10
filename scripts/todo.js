b670d30730c4872dca3ddc142c9f0c89e35724fb
scottgc <scott.gc@gmail.com>
2020-11-09 21:37:02 -0700

    Add items to a list.

--- scripts/todo.js
+++ scripts/todo.js
@@ -5,7 +5,15 @@ function sendAlert(message) {
 function addTodoItem() {
   var todo = document.getElementById("todo-item");
 
-  this.sendAlert(todo.value);
+  this.addToList(todo.value);
 
   todo.value = null
+}
+
+function addToList(message) {
+  var list = document.getElementById("todo-list");
+  var item = document.createElement("li");
+
+  item.appendChild(document.createTextNode(message));
+  list.appendChild(item);
 }
